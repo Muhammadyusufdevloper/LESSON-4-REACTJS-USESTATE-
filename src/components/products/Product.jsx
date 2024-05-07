3dc6859bfb86f5674f2963c0d3cdc0b24990b662
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-05-07 22:50:07 +0500

    homework

---
+++ src/components/products/Product.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./Product.scss"
+import { productData } from '../../static'
+
+const Product = () => {
+    const product = productData?.map((product)=>(
+        <div className='product__card' key={product.id}>
+            <div className='product__card-img'>
+                <img src={product.img} alt="" />
+            </div>
+            <div className='product__card-info'>
+                <h3 className='product__card-info__title'>{product.title}</h3>
+                <img src={product.start} alt="star img" />
+                <div className='product__card-info__part'>
+                    <p className='product__card-info__old-price'>{product.oldPrice}</p>
+                    <p className='product__card-info__new-price'>${product.newPrice}</p>
+                </div>
+            </div>
+        </div>
+    ))
+  return (
+    <>
+    <section className='product'>
+        <div className='container product__wrapper'>
+            <p className='product__desc'>Find your favourite smart watch.</p>
+            <h2 className='product__title'>Our Latest Products</h2>
+            <div className='product__cards'>
+                {product}
+            </div>
+            <button className='product__btn'>View More</button>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Product
